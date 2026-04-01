cea472e1296bc63be225855f95aa5b5fc34cc276
Gleb Pivushev <gpivusev@gmail.com>
2026-04-01 14:21:31 +0300

    реализация регистрации вызова, тесты

--- app/switchboard.py
+++ app/switchboard.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 from app.users import User
+from app.users import LocalUser
+from app.users import ForeignUser
+
 
 
 LOCAL_PHONE_PREFIX = "+7"
@@ -21,7 +24,9 @@ class ActiveCall:
 class Switchboard:
     def __init__(self) -> None:
         self._active_calls: list[ActiveCall] = []
+        self._cross_border_calls = 0 #чтобы вычислять колво иногородних звонков
 
+    #регистрация звонка
     def register_call(self, raw_call: str) -> ActiveCall:
         '''
         Метод должен принимать только 1 строку и возвращать класс ActiveCall.
@@ -29,10 +34,47 @@ class Switchboard:
 
         Например: "1001,Иван Петров,+71234567890,1085,Адам Яковлев,+71255556666"
         '''
-        pass  # Удалите `pass` и пишите ваш код
+
+        #распарсить строку в два объекта LocalUser и ForeignUser
+        #создать и добавить в лист ActiveCall
+        params = raw_call.split(",")
+
+        caller = None
+        receiver = None
+
+        #создаем callera и receivera
+        if (params[2].startswith("+7")):
+            caller = LocalUser(id=int(params[0]), fullname=params[1], phone=params[2])
+        else:
+            caller = ForeignUser(id=int(params[0]), fullname=params[1], phone=params[2])
+
+        if (params[5].startswith("+7")):
+            receiver = LocalUser(id=int(params[3]), fullname=params[4], phone=params[5])
+        else:
+           receiver = ForeignUser(id=int(params[3]), fullname=params[4], phone=params[5])
+
+        #добавляем в лист активных звонков текущий вызов
+        activeCall = ActiveCall(caller, receiver)
+        if activeCall.is_cross_border:
+            self._cross_border_calls += 1
+
+        self._active_calls.append(activeCall)
+        return activeCall
+
+    #удаление звонка
+    def unregister_call(self, call: ActiveCall):
+        if call.is_cross_border:
+            self._cross_border_calls -= 1
+        self._active_calls.remove(call)
+
+    def unregister_last_call(self):
+        call = self._active_calls.pop()
+        if call.is_cross_border:
+            self._cross_border_calls -= 1
+
 
     def get_active_calls_count(self) -> int:
-        pass  # Удалите `pass` и пишите ваш код
+        return len(self._active_calls)
 
     def get_cross_border_calls_count(self) -> int:
-        pass  # Удалите `pass` и пишите ваш код
+        return self._cross_border_calls
--- tests/test_switchboard.py
+++ tests/test_switchboard.py
@@ -45,3 +45,27 @@ def test_register_call_counts_calls_between_local_and_foreign_users() -> None:
 
     assert switchboard.get_active_calls_count() == 3
     assert switchboard.get_cross_border_calls_count() == 1
+
+
+def test_register_and_unregister_call() -> None:
+    switchboard = Switchboard()
+
+    call1 = switchboard.register_call(
+        "1,Chuck Norris,+79990000000,2,Kurt Cobain,+15551234567"
+    )
+
+    assert switchboard.get_cross_border_calls_count() == 1
+
+    switchboard.unregister_call(call1)
+    assert switchboard.get_cross_border_calls_count() == 0
+
+    switchboard.register_call(
+        "3,Jacque Fresco,+78880000000,4,Robert Oppenheimer,+79991112233"
+    )
+    switchboard.register_call(
+        "5,Keanu Reeves,+33123456789,6,Thomas Hanks,+442012345678"
+    )
+    assert switchboard.get_active_calls_count() == 2
+
+    switchboard.unregister_last_call()
+    assert switchboard.get_active_calls_count() == 1
